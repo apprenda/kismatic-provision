ce43331fa06b70694eaee31524390836289461fa
Sasha <sjeltuhin@apprenda.com>
2017-02-09 12:04:03 -0500

    Changed package reference

--- provision/exec/provision-cmd/main.go
+++ provision/exec/provision-cmd/main.go
@@ -4,9 +4,9 @@ import (
 	"os"
 
 	"github.com/apprenda/kismatic-provision/provision/aws"
+	"github.com/apprenda/kismatic-provision/provision/openstack"
 	"github.com/apprenda/kismatic-provision/provision/packet"
 	"github.com/apprenda/kismatic-provision/provision/vagrant"
-	"github.com/sashajeltuhin/ket/provision/openstack"
 	"github.com/spf13/cobra"
 )
 
