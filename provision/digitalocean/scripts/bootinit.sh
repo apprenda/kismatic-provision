b3e421fc85eaac880b694b925e176843989cd32b
Sasha <sjeltuhin@apprenda.com>
2017-03-08 22:01:39 -0500

    Load python in init script

--- provision/digitalocean/scripts/bootinit.sh
+++ provision/digitalocean/scripts/bootinit.sh
@@ -2,4 +2,5 @@ mkdir -p /ket &&
 cd /ket &&
 curl -L https://github.com/apprenda/kismatic/releases/download/v1.2.0/kismatic-v1.2.0-linux-amd64.tar.gz | tar -zx && 
 sudo apt-get -y install git build-essential &&
+sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python &&
 git clone https://github.com/sashajeltuhin/kubernetes-workshop.git
