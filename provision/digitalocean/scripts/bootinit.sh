178637adcdc11c541eb51d128f9baebd6ed4ebd7
Sasha <sjeltuhin@apprenda.com>
2017-03-09 00:19:51 -0500

    Fixes to init script

--- provision/digitalocean/scripts/bootinit.sh
+++ provision/digitalocean/scripts/bootinit.sh
@@ -1,6 +1,6 @@
 mkdir -p /ket &&
 cd /ket &&
-sudo apt-get update &&
+sudo apt-get update -y &&
 curl -L https://github.com/apprenda/kismatic/releases/download/v1.2.0/kismatic-v1.2.0-linux-amd64.tar.gz | tar -zx && 
 sudo apt-get -y install git build-essential &&
 sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python &&
